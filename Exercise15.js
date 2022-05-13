a25f07180dba497cb6aa5989e3dc0d98dc53fde6
Ranjana Galangco <97892742+RanjanaGalangco@users.noreply.github.com>
2022-05-13 11:53:59 +0800

    Add files via upload

---
+++ Exercise15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "Ranjana Galangco";
+    var age = document.getElementById("age").value = "20";
+    var phoneNumber = document.getElementById("phoneNumber").value = "09266536902";
+    var eMail = document.getElementById("eMail").value = "20070225@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "General Luna, Baguio City";
+    var program = document.getElementById("program").value = "Computer Science";
+    var idNum = document.getElementById("idNum").value = "20070225";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA2";
+}
